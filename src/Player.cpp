bba9cfc96d96ff08d48f0596ac164a8278db1390
Jin3314 <56128184+Jin3314@users.noreply.github.com>
2020-10-24 21:13:38 +0900

    lib,src-A2

---
+++ src/Player.cpp
@@ -0,0 +1,88 @@
+#include "Player.h"
+#include "TextureManager.h"
+
+Player::Player()
+{
+	TextureManager::Instance()->load("../Assets/textures/crate.png", "crate");
+
+	const auto size = TextureManager::Instance()->getTextureSize("crate");
+	setWidth(size.x);
+	setHeight(size.y);
+	getTransform()->position = glm::vec2(300.0f, 300.0f);
+	getRigidBody()->velocity = glm::vec2(0, 0);
+	getRigidBody()->isColliding = false;
+	setType(PLAYER);
+
+	//m_buildAnimations();
+}
+
+Player::~Player()
+= default;
+
+void Player::draw()
+{
+	// alias for x and y
+	const auto x = getTransform()->position.x;
+	const auto y = getTransform()->position.y;
+
+	/*// draw the player according to animation state
+	switch(m_currentAnimationState)
+	{
+	case PLAYER_IDLE_RIGHT:
+		TextureManager::Instance()->playAnimation("spritesheet", getAnimation("idle"),
+			x, y, 0.12f, 0, 255, true);
+		break;
+	case PLAYER_IDLE_LEFT:
+		TextureManager::Instance()->playAnimation("spritesheet", getAnimation("idle"),
+			x, y, 0.12f, 0, 255, true, SDL_FLIP_HORIZONTAL);
+		break;
+	case PLAYER_RUN_RIGHT:
+		TextureManager::Instance()->playAnimation("spritesheet", getAnimation("run"),
+			x, y, 0.25f, 0, 255, true);
+		break;
+	case PLAYER_RUN_LEFT:
+		TextureManager::Instance()->playAnimation("spritesheet", getAnimation("run"),
+			x, y, 0.25f, 0, 255, true, SDL_FLIP_HORIZONTAL);
+		break;
+	default:
+		break;
+	} */
+	TextureManager::Instance()->draw("crate", x, y, 0, 255, true);
+	
+}
+
+void Player::update()
+{
+}
+
+void Player::clean()
+{
+}
+
+void Player::setAnimationState(const PlayerAnimationState new_state)
+{
+	m_currentAnimationState = new_state;
+}
+
+void Player::m_buildAnimations()
+{
+	Animation idleAnimation = Animation();
+
+	idleAnimation.name = "idle";
+	idleAnimation.frames.push_back(getSpriteSheet()->getFrame("megaman-idle-0"));
+	idleAnimation.frames.push_back(getSpriteSheet()->getFrame("megaman-idle-1"));
+	idleAnimation.frames.push_back(getSpriteSheet()->getFrame("megaman-idle-2"));
+	idleAnimation.frames.push_back(getSpriteSheet()->getFrame("megaman-idle-3"));
+
+	setAnimation(idleAnimation);
+
+	Animation runAnimation = Animation();
+
+	runAnimation.name = "run";
+	runAnimation.frames.push_back(getSpriteSheet()->getFrame("megaman-run-0"));
+	runAnimation.frames.push_back(getSpriteSheet()->getFrame("megaman-run-1"));
+	runAnimation.frames.push_back(getSpriteSheet()->getFrame("megaman-run-2"));
+	runAnimation.frames.push_back(getSpriteSheet()->getFrame("megaman-run-3"));
+
+	setAnimation(runAnimation);
+}
